3c7fb9a7e6129b712e646e3f91bd933517e25fbb
DESKTOP-6700SAC\Luis <luisptcolaco@gmail.com>
2021-01-06 22:31:25 +0000

    Post class and Home Page post previews

---
+++ src/app/model/Post.ts
@@ -0,0 +1,8 @@
+export class Post {
+  title: string = '';
+  body: string = '';
+  summary: string = '';
+  coverImgSrc: string = '';
+  datePublished: Date = new Date();
+  tags: string[] = [];
+}
